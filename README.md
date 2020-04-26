51118579c98ee257a30e5f388af360e28eb8f7e2
Kevin Smeraglio <ksmera01@gmail.com>
2020-04-26 15:47:51 -0400

    updated more output in generate js, wrote more inputs to readme

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 
-  # Test
+  # ReadMe Generator
+
+  # This is a description
+
+  # Here is a link
+
+  # Make sure to run an npm install
+
+  # Please answer each question with detail.
+
+  # Need to figure out this selection
+
+  # None
+
+  # npm test
+
+  # Questions?: Feel free to contact me at ksmera01@gmail.com
   
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,22 @@
 function generateMarkdown(data) {
-    return `
+  return `
   # ${data.title}
+
+  # ${data.description}
+
+  # ${data.githubLink}
+
+  # ${data.installation}
+
+  # ${data.usage}
+
+  # ${data.license}
+
+  # ${data.contribute}
+
+  # ${data.tests}
+
+  # Questions?: Feel free to contact me at ksmera01@gmail.com
   
   `;
 }
